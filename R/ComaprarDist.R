db8074b60a195fb47de63bd5baed86fcbfa3cfe3
patriciacarvajal <patriciacarvajal.mxli@gmail.com>
2018-08-02 17:58:54 -0700

    CompararDist.R modificado

--- R/ComaprarDist.R
+++ R/ComaprarDist.R
@@ -8,7 +8,7 @@
 
 #Vamos a correr el test para todas las lineas
 
-distribution_results<-function(mat1,mat2){
+distribution_results_distintos<-function(mat1,mat2){
   x=seq_len(nrow(mat1))###
   resultado=lapply(x, function(x) ks.test(mat1[x,],mat2[x,])$p.value)##distribution_test(mat1, mat2)
   #Si es 0 son diferentes.
@@ -16,6 +16,26 @@ distribution_results<-function(mat1,mat2){
   # Accesa a distintas e iguales.
   distintas<-which(y == 0)
   iguales<-which(y == 1)
-  return(iguales)
+  nombres<-row.names(mat1)
+  genes_distintos<-nombres[distintas]
+  return(genes_distintos)
 }
+
+####################################################################
+
+
+distribution_results_iguales<-function(mat1,mat2){
+  x=seq_len(nrow(mat1))###
+  resultado=lapply(x, function(x) ks.test(mat1[x,],mat2[x,])$p.value)##distribution_test(mat1, mat2)
+  #Si es 0 son diferentes.
+  y<-ifelse(as.numeric(resultado) < .01, 1, 0)
+  # Accesa a distintas e iguales.
+  distintas<-which(y == 0)
+  iguales<-which(y == 1)
+  nombres<-row.names(mat1)
+  genes_iguales<-nombres[iguales]
+  return(genes_iguales)
+}
+
+#################################################
 
